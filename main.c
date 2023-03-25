d028f7e5f70dd3edb827d291ce2fd32d0322ea2b
Andee <andeeolayinkascott@gmail.com>
2023-03-25 07:45:22 +0000

    Added new line to error statements

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
 
 	if (!file)
 	{
-		fprintf(stderr, "Error: Can't open file %s", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (1)

de792b73982d807086141958bccfd96b71740ba1
Andee <andeeolayinkascott@gmail.com>
2023-03-25 09:01:02 +0000

    pop prototype added

--- monty.h
+++ monty.h
@@ -64,6 +64,7 @@ int execute(char *content, stack_t **stack, unsigned int line_num, FILE *file);
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
 
 
 #endif /* _MONTY_H */

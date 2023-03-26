1283351338afe2f760684848490832f565ade5f4
Andee <andeeolayinkascott@gmail.com>
2023-03-26 18:34:54 +0000

    Betty compliant

--- op_funcs.c
+++ op_funcs.c
@@ -95,7 +95,7 @@ void _pop(stack_t **stack, unsigned int line_number)
 
 	temp = *stack;
 	if (temp == NULL)
-	{	
+	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		fclose(var.file);
 		free(var.content);
